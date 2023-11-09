65a3c4d3fcb10170f605fc1f9ecb16cdca82ab25
Wassim-Rached <wassimrached404@gmail.com>
2023-11-09 16:08:40 +0100

    change admin components to be inside admin module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,63 +2,58 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+// application based components
 import { AppComponent } from './app.component';
-import { HttpClientModule } from "@angular/common/http";
 import { HomeComponent } from './application/components/home/home.component';
 import { AboutUsComponent } from './application/components/about-us/about-us.component';
 import { JoinUsComponent } from './application/components/join-us/join-us.component';
 import { DevTeamComponent } from './application/components/dev-team/dev-team.component';
-import { ActivitiesFeedComponent } from './application/components/activities-feed/activities-feed.component';
-import { ActivityDetailsComponent } from './application/components/activity-details/activity-details.component';
-import { ProfileComponent } from './application/components/profile/profile.component';
 import { NavbarComponent } from './application/components/navbar/navbar.component';
 import { FooterComponent } from './application/components/footer/footer.component';
 import { DashboardComponent } from './application/components/dashboard/dashboard.component';
-import { RegistrationsListComponent } from './application/components/registrations-list/registrations-list.component';
-import { RegistrationManagmentComponent } from './application/components/registration-managment/registration-managment.component';
-import { RegistrationDetailsComponent } from './application/components/registration-details/registration-details.component';
+// activity based components
 import { ActivityOverViewComponent } from './application/components/activity-over-view/activity-over-view.component';
 import { ActivityListComponent } from './application/components/activity-list/activity-list.component';
-import { ActivityManagmentComponent } from './application/components/activity-managment/activity-managment.component';
+import { ActivitiesPageComponent } from './application/components/activities-page/activities-page.component';
+import { ActivitiesFeedComponent } from './application/components/activities-feed/activities-feed.component';
+// user based components
+import { ProfileComponent } from './application/components/profile/profile.component';
+import { AccountPageComponent } from './application/components/account-page/account-page.component';
+import { ChangePasswordComponent } from './application/components/change-password/change-password.component';
 import { SignInComponent } from './application/components/sign-in/sign-in.component';
 import { SignUpComponent } from './application/components/sign-up/sign-up.component';
-import { ChangePasswordComponent } from './application/components/change-password/change-password.component';
+// error based components
 import { Error404Component } from './application/components/error404/error404.component';
-import { ActivitiesPageComponent } from './application/components/activities-page/activities-page.component';
-import { AccountPageComponent } from './application/components/account-page/account-page.component';
 
 @NgModule({
   declarations: [
+    // application based components
     AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    DashboardComponent,
     HomeComponent,
     AboutUsComponent,
     JoinUsComponent,
     DevTeamComponent,
+    // activity based components
     ActivitiesFeedComponent,
-    ActivityDetailsComponent,
-    ProfileComponent,
-    NavbarComponent,
-    FooterComponent,
-    DashboardComponent,
-    RegistrationsListComponent,
-    RegistrationManagmentComponent,
-    RegistrationDetailsComponent,
-    ActivityOverViewComponent,
     ActivityListComponent,
-    ActivityManagmentComponent,
-    SignInComponent,
-    SignUpComponent,
-    ChangePasswordComponent,
-    Error404Component,
+    ActivityOverViewComponent,
     ActivitiesPageComponent,
-    AccountPageComponent
+    // user based components
+    AccountPageComponent,
+    ChangePasswordComponent,
+    SignInComponent,
+    ProfileComponent,
+    SignUpComponent,
+    // error based components
+    Error404Component,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/application/admin/admin.module.ts
+++ src/app/application/admin/admin.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AdminRoutingModule } from './admin-routing.module';
 
+// register components
+import { RegistrationsListComponent } from './components/registrations-list/registrations-list.component';
+import { RegistrationManagmentComponent } from './components/registration-managment/registration-managment.component';
+import { RegistrationDetailsComponent } from './components/registration-details/registration-details.component';
+//
+import { ActivityManagmentComponent } from './components/activity-managment/activity-managment.component';
+import { ActivityDetailsComponent } from './components/activity-details/activity-details.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    AdminRoutingModule
-  ]
+  declarations: [
+    // register based components
+    RegistrationsListComponent,
+    RegistrationManagmentComponent,
+    RegistrationDetailsComponent,
+    // activity based components
+    ActivityDetailsComponent,
+    ActivityManagmentComponent,
+  ],
+  imports: [CommonModule, AdminRoutingModule],
 })
-export class AdminModule { }
+export class AdminModule {}
