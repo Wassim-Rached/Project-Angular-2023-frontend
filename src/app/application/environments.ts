e18f8c41a03fa6a2825d93a7762f2e01d5aa9e2c
Wassim-Rached <wassimrached404@gmail.com>
2023-11-11 00:03:00 +0100

    Update environment variables

--- src/app/application/environments.ts
+++ src/app/application/environments.ts
@@ -1,4 +1,7 @@
 export const environment = {
-  BASE_API_URL: 'https://mini-project-iset.onrender.com/api/',
   // BASE_API_URL: 'http://localhost:8000/api/',
+  BASE_API_URL: 'https://mini-project-iset.onrender.com/api/',
+  DEFAULT_REDIRECT_AFTER_LOGIN: '/',
+  DEFAULT_REDIRECT_AFTER_LOGOUT: '/',
+  DEFAULT_REDIRECT_FORBIDDEN: '/',
 };
