e073f5ddad908ecd330b005b16f1d4697bd578a7
Wassim-Rached <wassimrached404@gmail.com>
2023-11-11 14:44:58 +0100

    change dashboard component to admin module

--- src/app/application/admin/admin.module.ts
+++ src/app/application/admin/admin.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 
+// dashboard component
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 // register components
 import { RegistrationsListComponent } from './components/registrations-list/registrations-list.component';
 import { RegistrationManagmentComponent } from './components/registration-managment/registration-managment.component';
 import { RegistrationDetailsComponent } from './components/registration-details/registration-details.component';
-//
+// activity components
 import { ActivityManagmentComponent } from './components/activity-managment/activity-managment.component';
 import { ActivityDetailsComponent } from './components/activity-details/activity-details.component';
 
 @NgModule({
   declarations: [
+    // dashboard component
+    DashboardComponent,
     // register based components
     RegistrationsListComponent,
     RegistrationManagmentComponent,
