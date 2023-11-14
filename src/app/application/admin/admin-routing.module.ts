a0c600ccc3c4a08d9acda58dc6f0ba53f523e57b
Wassim-Rached <wassimrached404@gmail.com>
2023-11-15 00:06:35 +0100

    Add activity and registration management routes to admin module

--- src/app/application/admin/admin-routing.module.ts
+++ src/app/application/admin/admin-routing.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { GreetingAdminComponent } from './components/greeting-admin/greeting-admin.component';
+import { ActivityManagmentComponent } from './components/activity-managment/activity-managment.component';
+import { ActivityDetailsComponent } from './components/activity-details/activity-details.component';
+import { ActivityListAdminComponent } from './components/activity-list-admin/activity-list-admin.component';
+import { ActivityCreateAdminComponent } from './components/activity-create-admin/activity-create-admin.component';
+import { RegistrationManagmentComponent } from './components/registration-managment/registration-managment.component';
+import { RegistrationsListComponent } from './components/registrations-list/registrations-list.component';
 
 const routes: Routes = [
   {
@@ -11,6 +17,42 @@ const routes: Routes = [
     children: [
       { path: '', redirectTo: 'greeting', pathMatch: 'full' },
       { path: 'greeting', title: 'welcome', component: GreetingAdminComponent },
+      {
+        path: 'activities',
+        title: 'activities',
+        component: ActivityManagmentComponent,
+        children: [
+          { path: '', redirectTo: 'list', pathMatch: 'full' },
+          {
+            path: 'list',
+            title: 'activity list',
+            component: ActivityListAdminComponent,
+          },
+          {
+            path: 'create',
+            title: 'activity create',
+            component: ActivityCreateAdminComponent,
+          },
+          {
+            path: ':activityId',
+            title: 'activity details',
+            component: ActivityDetailsComponent,
+          },
+        ],
+      },
+      {
+        path: 'registrations',
+        title: 'registrations',
+        component: RegistrationManagmentComponent,
+        children: [
+          { path: '', redirectTo: 'list', pathMatch: 'full' },
+          {
+            path: 'list',
+            title: 'registration list',
+            component: RegistrationsListComponent,
+          },
+        ],
+      },
     ],
   },
 ];
