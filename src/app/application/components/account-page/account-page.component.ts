14468b8eacbaffac2c5902bbc1c20168a08572c2
Wassim-Rached <wassimrached404@gmail.com>
2023-11-15 00:07:33 +0100

    Refactor account page component to display profile information

--- src/app/application/components/account-page/account-page.component.ts
+++ src/app/application/components/account-page/account-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../../services/account.service';
 import { Account } from '../../classes/account';
 import { AuthService } from '../../services/auth.service';
-import { ActivatedRoute, Route, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-account-page',
@@ -12,28 +12,35 @@ import { ActivatedRoute, Route, Router } from '@angular/router';
 export class AccountPageComponent implements OnInit {
   isOwner: boolean = false;
   account!: Account;
+  profileId!: string;
+
   constructor(
     private accountService: AccountService,
     private authService: AuthService,
     private activatedRoute: ActivatedRoute
   ) {}
+
   ngOnInit(): void {
-    this.accountService.getMyAccount().subscribe({
+    // get the profile id from the url
+    this.profileId = this.activatedRoute.snapshot.params['profileId'];
+
+    // get the account of the profile by id
+    this.accountService.getAccountById(this.profileId).subscribe({
       next: (account) => {
+        // set the account of the profile
         this.account = account;
-        const profileId = this.activatedRoute.snapshot.params['profileId'];
-        this.isOwner = profileId === account.id;
-        console.log(profileId);
-        console.log(account.id);
+        // get the current account id
+        const currentAccountId = this.authService.getAccountId();
+        // check if the current account is the owner of the profile
+        this.isOwner = currentAccountId === account.id;
       },
     });
   }
+
   onDelete() {
     this.accountService.deleteAccountById(this.account.id!).subscribe({
       next: (data) => {
-        console.log(data);
         this.authService.logout();
-        this.authService.redirectAfterLogout();
       },
     });
   }
