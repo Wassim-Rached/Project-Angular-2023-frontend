53d83feeddb118d9543e1ca87ba6c628b0e87090
chaima chouikh <shouikhsheimaa@gmail.com>
2023-11-14 17:19:33 +0100

    fixed some in some components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { SignUpComponent } from './application/components/sign-up/sign-up.compon
 import { JoinUsComponent } from './application/components/join-us/join-us.component';
 import { DevTeamComponent } from './application/components/dev-team/dev-team.component';
 import { isAdminGuard } from './application/guards/is-admin.guard';
+import { isUnauthenticatedGuard } from './application/guards/is-unauthenticated.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -44,11 +45,11 @@ const routes: Routes = [
   },
   { path: 'join-us', title: 'join us', component: JoinUsComponent },
   {
-    path: 'profile',
+    path: 'profile/:profileId',
     title: 'profile',
     component: AccountPageComponent,
     children: [
-      { path: ':profileId', title: 'Profile', component: ProfileComponent },
+      { path: '', title: 'Profile', component: ProfileComponent },
       {
         path: 'change-password',
         title: 'change password',
@@ -57,7 +58,12 @@ const routes: Routes = [
     ],
   },
   { path: 'dev-team', title: 'dev-team', component: DevTeamComponent },
-  { path: 'signin', title: 'signin', component: SignInComponent },
+  {
+    path: 'signin',
+    title: 'signin',
+    canActivate: [isUnauthenticatedGuard],
+    component: SignInComponent,
+  },
   { path: 'signup', title: 'signup', component: SignUpComponent },
   { path: '**', title: 'NOT FOUND 404! ', component: Error404Component },
 ];
