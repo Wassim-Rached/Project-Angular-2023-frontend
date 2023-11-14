2617e2e2604c5a64bf63055116d1b0b37b88983e
chaima chouikh <shouikhsheimaa@gmail.com>
2023-11-14 17:19:03 +0100

    working on account page

--- src/app/application/components/account-page/account-page.component.ts
+++ src/app/application/components/account-page/account-page.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../../services/account.service';
+import { Account } from '../../classes/account';
+import { AuthService } from '../../services/auth.service';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-account-page',
   templateUrl: './account-page.component.html',
-  styleUrls: ['./account-page.component.css']
+  styleUrls: ['./account-page.component.css'],
 })
-export class AccountPageComponent {
-
+export class AccountPageComponent implements OnInit {
+  isOwner: boolean = false;
+  account!: Account;
+  constructor(
+    private accountService: AccountService,
+    private authService: AuthService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    this.accountService.getMyAccount().subscribe({
+      next: (account) => {
+        this.account = account;
+        const profileId = this.activatedRoute.snapshot.params['profileId'];
+        this.isOwner = profileId === account.id;
+        console.log(profileId);
+        console.log(account.id);
+      },
+    });
+  }
+  onDelete() {
+    this.accountService.deleteAccountById(this.account.id!).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.authService.logout();
+        this.authService.redirectAfterLogout();
+      },
+    });
+  }
 }
