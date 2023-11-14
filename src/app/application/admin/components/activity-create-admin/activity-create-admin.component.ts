8d8727eb36c35ca5f4e9c6f4f5bc159908f1e5b9
Wassim-Rached <wassimrached404@gmail.com>
2023-11-15 00:06:48 +0100

    Add activity-create-admin component.

---
+++ src/app/application/admin/components/activity-create-admin/activity-create-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-create-admin',
+  templateUrl: './activity-create-admin.component.html',
+  styleUrls: ['./activity-create-admin.component.css']
+})
+export class ActivityCreateAdminComponent {
+
+}
