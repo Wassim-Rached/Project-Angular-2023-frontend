9fad7ff30ad3e80baa159bb5d9fd35252cf2a1a1
Wassim-Rached <wassimrached404@gmail.com>
2023-11-15 00:06:56 +0100

    Add activity-list-admin component.

---
+++ src/app/application/admin/components/activity-list-admin/activity-list-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-list-admin',
+  templateUrl: './activity-list-admin.component.html',
+  styleUrls: ['./activity-list-admin.component.css']
+})
+export class ActivityListAdminComponent {
+
+}
