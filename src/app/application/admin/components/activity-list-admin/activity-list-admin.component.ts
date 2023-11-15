58ec42aeaf21ecf83743b847da92ed8a1717beb1
chaima chouikh <shouikhsheimaa@gmail.com>
2023-11-15 15:44:43 +0100

    worked on activity list admin component

--- src/app/application/admin/components/activity-list-admin/activity-list-admin.component.ts
+++ src/app/application/admin/components/activity-list-admin/activity-list-admin.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Activity } from 'src/app/application/classes/activity';
+import { ActivityService } from 'src/app/application/services/activity.service';
 
 @Component({
   selector: 'app-activity-list-admin',
   templateUrl: './activity-list-admin.component.html',
-  styleUrls: ['./activity-list-admin.component.css']
+  styleUrls: ['./activity-list-admin.component.css'],
 })
-export class ActivityListAdminComponent {
+export class ActivityListAdminComponent implements OnInit {
+  activities: Activity[] = [];
+  Form!: FormGroup;
+  constructor(
+    private activityService: ActivityService,
+    private fb: FormBuilder
+  ) {}
 
+  onSubmit() {
+    const { title__icontains, ordering } = this.Form.value;
+    const params = new HttpParams();
+    params.set('title__icontains', title__icontains);
+    params.set('ordering', ordering);
+    this.activityService.getAllActivities(params).subscribe({
+      next: (activities) => {
+        this.activities = activities;
+      },
+    });
+  }
+  ngOnInit(): void {
+    this.Form = this.fb.nonNullable.group({
+      title__icontains: [''],
+      ordering: ['created_at'],
+    });
+    this.activityService.getAllActivities().subscribe({
+      next: (activities) => {
+        this.activities = activities;
+      },
+    });
+  }
 }
