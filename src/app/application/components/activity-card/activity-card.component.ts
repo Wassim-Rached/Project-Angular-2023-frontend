e4c49b728a115a41613865afda99a2b197628bf7
chaima chouikh <shouikhsheimaa@gmail.com>
2023-11-18 14:22:00 +0100

    done the like foncionnality

--- src/app/application/components/activity-card/activity-card.component.ts
+++ src/app/application/components/activity-card/activity-card.component.ts
@@ -1,14 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Activity } from '../../classes/activity';
 import { ActivityService } from '../../services/activity.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-activity-card',
   templateUrl: './activity-card.component.html',
   styleUrls: ['./activity-card.component.css'],
 })
-export class ActivityCardComponent {
+export class ActivityCardComponent implements OnInit {
   @Input() activity!: Activity;
+  public isAuthenticated!: boolean;
 
-  constructor(private activityService: ActivityService) {}
+  constructor(
+    private activityService: ActivityService,
+    private authService: AuthService
+  ) {}
+  ngOnInit(): void {
+    this.isAuthenticated = this.authService.isAuthenticated();
+  }
+
+  public onLike() {
+    if (!this.isAuthenticated) return;
+
+    this.activityService.setLiked(this.activity.id!).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.activity.number_of_likes! += 1;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
