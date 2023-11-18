fedbda46fcee79095635d64d11a1653b54212e29
chaima chouikh <shouikhsheimaa@gmail.com>
2023-11-18 17:22:47 +0100

    finished registration fonctinality

--- src/app/application/classes/registration.ts
+++ src/app/application/classes/registration.ts
@@ -1,15 +1,15 @@
-import { Account } from "./account";
-import { Activity } from "./activity";
-import { Status } from "./join-us";
+import { Account } from './account';
+import { Activity } from './activity';
+import { Status } from './join-us';
 
 export class Registration {
-    constructor(
-        public readonly id?: string,
-        public status?: Status,
-        public is_payed?: boolean,
-        public readonly created_at?: string,
-        public readonly updated_at?: string,
-        public readonly account?: Account,
-        public activity?: Activity
-    ) { }
+  constructor(
+    public readonly id?: string,
+    public status?: Status,
+    public is_payed?: boolean,
+    public readonly created_at?: string,
+    public readonly updated_at?: string,
+    public readonly account?: Account,
+    public activity?: Activity
+  ) {}
 }
