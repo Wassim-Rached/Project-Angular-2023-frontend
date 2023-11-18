8f0d9fa789ec9c9f1edece8884761642ba1148e9
chaima chouikh <shouikhsheimaa@gmail.com>
2023-11-18 14:21:03 +0100

    added loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from './application/environments';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  backendUp: boolean = false;
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    const URL = environment['BASE_API_URL'] + 'activities/';
+
+    this.http.get(URL).subscribe({
+      next: (res) => {
+        this.backendUp = true;
+      },
+      error: (Error) => {
+        console.log(Error);
+      },
+    });
+  }
+}
