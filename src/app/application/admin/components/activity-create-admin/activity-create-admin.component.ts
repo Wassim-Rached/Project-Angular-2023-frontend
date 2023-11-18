6f32b767faf2deed7b5a76376f16ba598151ce43
chaima chouikh <shouikhsheimaa@gmail.com>
2023-11-18 15:00:57 +0100

    finished form for activity creation

--- src/app/application/admin/components/activity-create-admin/activity-create-admin.component.ts
+++ src/app/application/admin/components/activity-create-admin/activity-create-admin.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ActivityService } from 'src/app/application/services/activity.service';
 
 @Component({
   selector: 'app-activity-create-admin',
   templateUrl: './activity-create-admin.component.html',
-  styleUrls: ['./activity-create-admin.component.css']
+  styleUrls: ['./activity-create-admin.component.css'],
 })
-export class ActivityCreateAdminComponent {
+export class ActivityCreateAdminComponent implements OnInit {
+  form!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private activityService: ActivityService
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.nonNullable.group({
+      title: [''],
+      photo: [null],
+      isFree: [true],
+      price: [''],
+      maxParticipants: [''],
+      date: [''],
+      description: [''],
+    });
+  }
+  resetForm() {
+    this.form.reset();
+  }
+  onSubmit() {
+    const data = this.form.value;
+    delete data.photo;
+    console.log(data);
+    this.activityService.createActivity(data).subscribe({
+      next: (data) => {
+        this.resetForm();
+        alert('activity created successfully');
+      },
+      error: (error) => {
+        alert('activity not created !! try again later');
+      },
+    });
+  }
 }
