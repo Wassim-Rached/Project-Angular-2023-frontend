7d171b515bd5ffeccf7ed715387359d691d6db28
chaima chouikh <shouikhsheimaa@gmail.com>
2023-11-19 10:56:37 +0100

    finished the chnage password fonctionality

--- src/app/application/components/change-password/change-password.component.ts
+++ src/app/application/components/change-password/change-password.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AccountService } from '../../services/account.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.css'],
 })
-export class ChangePasswordComponent {}
+export class ChangePasswordComponent implements OnInit {
+  Form!: FormGroup;
+  @Input() profileId!: string;
+
+  constructor(
+    private accountService: AccountService,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.Form = this.fb.nonNullable.group({
+      old_password: [''],
+      new_password: [''],
+    });
+  }
+  onSubmit() {
+    const { old_password, new_password } = this.Form.value;
+    this.accountService
+      .changePassword(this.profileId, old_password, new_password)
+      .subscribe({
+        next: (res) => {
+          alert('password changed !');
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+  }
+}
