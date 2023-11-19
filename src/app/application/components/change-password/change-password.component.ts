aa5ba36efeadfdcb6210e5a1b1b92ede77f88da4
chaima chouikh <shouikhsheimaa@gmail.com>
2023-11-19 11:10:25 +0100

    finished password error handling

--- src/app/application/components/change-password/change-password.component.ts
+++ src/app/application/components/change-password/change-password.component.ts
@@ -10,6 +10,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class ChangePasswordComponent implements OnInit {
   Form!: FormGroup;
   @Input() profileId!: string;
+  isSubmitting: boolean = false;
 
   constructor(
     private accountService: AccountService,
@@ -23,16 +24,37 @@ export class ChangePasswordComponent implements OnInit {
     });
   }
   onSubmit() {
+    this.isSubmitting = true;
     const { old_password, new_password } = this.Form.value;
     this.accountService
       .changePassword(this.profileId, old_password, new_password)
       .subscribe({
         next: (res) => {
+          this.isSubmitting = false;
           alert('password changed !');
         },
         error: (error) => {
-          console.log(error);
+          this.isSubmitting = false;
+          if (error.status === 400) this.handleFormErrors(error.error);
         },
       });
+  }
+
+  private handleFormErrors(errors: any): void {
+    // set the errors from the backend
+    // on the form controls
+    for (const field in errors) {
+      if (this.Form.get(field)) {
+        this.Form.get(field)?.setErrors({ serverError: errors[field][0] });
+      }
+    }
+  }
+
+  public fieldHasError(field: string): boolean {
+    return this.Form.get(field)?.hasError('serverError') ?? true;
+  }
+
+  public getFieldErrorMessage(field: string): string {
+    return this.Form.get(field)?.errors?.['serverError'];
   }
 }
