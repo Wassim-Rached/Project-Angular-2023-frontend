0087fcdc64cfed95790ee6d83c3e44d54a88f90a
Wassim-Rached <wassimrached404@gmail.com>
2023-11-19 23:16:28 +0100

    Delete activities-page component

--- src/app/application/components/activities-page/activities-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-activities-page',
-  templateUrl: './activities-page.component.html',
-  styleUrls: ['./activities-page.component.css'],
-})
-export class ActivitiesPageComponent {}
