9e687f6d9176620ea33c867cf359f81984ff8222
Wassim-Rached <wassimrached404@gmail.com>
2023-11-21 23:31:34 +0100

    Refactor account-page component to include loading state

--- src/app/application/components/account-page/account-page.component.ts
+++ src/app/application/components/account-page/account-page.component.ts
@@ -13,6 +13,7 @@ export class AccountPageComponent implements OnInit {
   isOwner: boolean = false;
   account!: Account;
   profileId!: string;
+  isLoadingPage = false;
 
   constructor(
     private accountService: AccountService,
@@ -21,6 +22,9 @@ export class AccountPageComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    // set the loading page to true
+    this.isLoadingPage = true;
+
     // get the profile id from the url
     this.profileId = this.activatedRoute.snapshot.params['profileId'];
 
@@ -33,6 +37,12 @@ export class AccountPageComponent implements OnInit {
         const currentAccountId = this.authService.getAccountId();
         // check if the current account is the owner of the profile
         this.isOwner = currentAccountId === account.id;
+        // set the loading page to false
+        this.isLoadingPage = false;
+      },
+      error: (error) => {
+        // set the loading page to false
+        this.isLoadingPage = false;
       },
     });
   }
