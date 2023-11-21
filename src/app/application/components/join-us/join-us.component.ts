2391b243120bf506adb4bf3279d9a69d04d3e651
chaima chouikh <shouikhsheimaa@gmail.com>
2023-11-21 11:53:26 +0100

    completed the join us form

--- src/app/application/components/join-us/join-us.component.ts
+++ src/app/application/components/join-us/join-us.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../../services/account.service';
+import { Account } from '../../classes/account';
+import { JoinUs } from '../../classes/join-us';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-join-us',
   templateUrl: './join-us.component.html',
-  styleUrls: ['./join-us.component.css']
+  styleUrls: ['./join-us.component.css'],
 })
-export class JoinUsComponent {
+export class JoinUsComponent implements OnInit {
+  Form!: FormGroup;
+  join?: JoinUs;
+  constructor(
+    private accountService: AccountService,
+    private fb: FormBuilder
+  ) {}
+  ngOnInit(): void {
+    this.accountService.getAllJoiningForms().subscribe({
+      next: (data) => {
+        this.join = data[0];
+        console.log(this.join);
+      },
+    });
 
+    this.Form = this.fb.nonNullable.group({
+      findUs: ['', Validators.required],
+      goals: ['', Validators.required],
+      reasons: ['', Validators.required],
+      recieveEmails: [true],
+    });
+  }
+  public haveRequiredError(field: string) {
+    return (
+      this.Form.get(field)?.errors?.['required'] &&
+      this.Form.get(field)?.touched
+    );
+  }
+
+  public JoinClub() {
+    this.accountService.joinClub(this.Form.value).subscribe({
+      next: (data) => alert('Successfully joined club'),
+      error: () => alert('Failed to join club'),
+    });
+  }
 }
