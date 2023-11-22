a1b1f11be123951d242c3cb0f3af8f4215b8b095
chaima chouikh <shouikhsheimaa@gmail.com>
2023-11-22 13:43:33 +0100

    working on the details component of the admin

--- src/app/application/admin/components/join-us-details-admin/join-us-details-admin.component.ts
+++ src/app/application/admin/components/join-us-details-admin/join-us-details-admin.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { JoinUs } from 'src/app/application/classes/join-us';
+import { AccountService } from 'src/app/application/services/account.service';
 
 @Component({
   selector: 'app-join-us-details-admin',
   templateUrl: './join-us-details-admin.component.html',
-  styleUrls: ['./join-us-details-admin.component.css']
+  styleUrls: ['./join-us-details-admin.component.css'],
 })
-export class JoinUsDetailsAdminComponent {
-
+export class JoinUsDetailsAdminComponent implements OnInit {
+  joinForm?: JoinUs;
+  constructor(
+    private accountService: AccountService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+  ngOnInit(): void {
+    const joinId = this.activatedRoute.snapshot.params['id'];
+    this.accountService.getJoinFormById(joinId).subscribe({
+      next: (joinForm) => {
+        this.joinForm = joinForm;
+      },
+    });
+  }
 }
