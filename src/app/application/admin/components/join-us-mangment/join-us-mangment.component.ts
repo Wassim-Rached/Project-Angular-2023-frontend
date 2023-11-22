28f7e76694d7168388a2aa1efa58e71418fca541
chaima chouikh <shouikhsheimaa@gmail.com>
2023-11-22 08:47:00 +0100

    added join us mangment

---
+++ src/app/application/admin/components/join-us-mangment/join-us-mangment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-us-mangment',
+  templateUrl: './join-us-mangment.component.html',
+  styleUrls: ['./join-us-mangment.component.css']
+})
+export class JoinUsMangmentComponent {
+
+}
