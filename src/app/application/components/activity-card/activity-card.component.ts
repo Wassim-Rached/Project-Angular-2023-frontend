5c569d4de5c2d8f57ba1bf35dd531434ab2b1077
Wassim-Rached <wassimrached404@gmail.com>
2023-11-23 23:13:55 +0100

    Add loading state to activity card component

--- src/app/application/components/activity-card/activity-card.component.ts
+++ src/app/application/components/activity-card/activity-card.component.ts
@@ -13,19 +13,37 @@ export class ActivityCardComponent implements OnInit {
   isAuthenticated!: boolean;
   didLike: boolean = false;
   isLoadingLike: boolean = false;
+  isPageLoading: boolean = false;
 
   constructor(
     private activityService: ActivityService,
     private authService: AuthService
   ) {}
+
   ngOnInit(): void {
+    // set the loading state to true
+    this.isPageLoading = true;
+
+    // set the authenticated state
     this.isAuthenticated = this.authService.isAuthenticated();
+
+    // if the user is authenticated
     if (this.isAuthenticated) {
       this.activityService.didLike(this.activity.id!).subscribe({
         next: (response) => {
+          // set the did like state
           this.didLike = response.did_like;
+          // set the loading state to false
+          this.isPageLoading = false;
+        },
+        error: (error) => {
+          // set the loading state to false
+          this.isPageLoading = false;
         },
       });
+    } else {
+      // set the loading state to false
+      this.isPageLoading = false;
     }
   }
 
@@ -33,14 +51,21 @@ export class ActivityCardComponent implements OnInit {
     if (this.didLike) return;
     if (!this.isAuthenticated) return;
 
+    // set the loading like state to true
     this.isLoadingLike = true;
+
     this.activityService.setLiked(this.activity.id!).subscribe({
       next: (response) => {
+        // increment the number of likes
+        // and set the did like state to true
         this.activity.number_of_likes! += 1;
         this.didLike = true;
+
+        // set the loading like state to false
         this.isLoadingLike = false;
       },
       error: (error) => {
+        // set the loading like state to false
         this.isLoadingLike = false;
       },
     });
