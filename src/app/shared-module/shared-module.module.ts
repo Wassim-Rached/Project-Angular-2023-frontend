ae0b27a6b9999aa1e4f773a57980450e9f67ce59
Wassim-Rached <wassimrached404@gmail.com>
2023-11-23 20:40:33 +0100

    Add MainLoaderComponent to SharedModuleModule

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { UserNameFormaterPipe } from '../application/pipes/user-name-formater.pipe';
+import { MainLoaderComponent } from '../application/components/main-loader/main-loader.component';
 
 @NgModule({
-  declarations: [UserNameFormaterPipe],
-  exports: [UserNameFormaterPipe],
+  declarations: [UserNameFormaterPipe,MainLoaderComponent],
+  exports: [UserNameFormaterPipe,MainLoaderComponent],
 })
 export class SharedModuleModule {}
