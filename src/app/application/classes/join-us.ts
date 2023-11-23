ddd964f1fc14495610f1d2d97f634526304832eb
Wassim-Rached <wassimrached404@gmail.com>
2023-11-23 23:35:36 +0100

    Add created_at and updated_at properties to JoinUs class

--- src/app/application/classes/join-us.ts
+++ src/app/application/classes/join-us.ts
@@ -3,6 +3,9 @@ import { Account } from './account';
 export type Status = 'pending' | 'accepted' | 'rejected';
 
 export class JoinUs {
+  public readonly created_at?: Date;
+  public readonly updated_at?: Date;
+
   constructor(
     public readonly id?: string,
     public readonly account?: Account,
@@ -10,6 +13,11 @@ export class JoinUs {
     public how_found_us?: string,
     public goals?: string,
     public reasons?: string,
-    public receive_emails?: boolean
-  ) {}
+    public receive_emails?: boolean,
+    created_at?: string,
+    updated_at?: string
+  ) {
+    if (created_at) this.created_at = new Date(created_at);
+    if (updated_at) this.updated_at = new Date(updated_at);
+  }
 }
