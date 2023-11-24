5e1d1c9ac020cdeffa6d6a082c5ec574f52914d9
Wassim-Rached <wassimrached404@gmail.com>
2023-11-24 22:22:20 +0100

    Add input error message component to shared module

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { UserNameFormaterPipe } from '../application/pipes/user-name-formater.pipe';
 import { MainLoaderComponent } from '../application/components/main-loader/main-loader.component';
+import { MessageTextComponent } from '../application/components/message-text/message-text.component';
+import { CommonModule } from '@angular/common';
+import { InputErrorMessageComponent } from '../application/components/input-error-message/input-error-message.component';
 
 @NgModule({
-  declarations: [UserNameFormaterPipe,MainLoaderComponent],
-  exports: [UserNameFormaterPipe,MainLoaderComponent],
+  declarations: [
+    UserNameFormaterPipe,
+    MainLoaderComponent,
+    MessageTextComponent,
+    InputErrorMessageComponent,
+  ],
+  imports: [CommonModule],
+  exports: [
+    UserNameFormaterPipe,
+    MainLoaderComponent,
+    MessageTextComponent,
+    InputErrorMessageComponent,
+  ],
 })
 export class SharedModuleModule {}
