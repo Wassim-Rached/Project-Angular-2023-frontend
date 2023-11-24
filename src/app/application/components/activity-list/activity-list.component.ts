0a1cdcd633d8e0e5ffff62abc3a86a931391d976
Wassim-Rached <wassimrached404@gmail.com>
2023-11-24 20:05:46 +0100

    add categorie search to activites list frontoffice

--- src/app/application/components/activity-list/activity-list.component.ts
+++ src/app/application/components/activity-list/activity-list.component.ts
@@ -3,6 +3,7 @@ import { Activity } from '../../classes/activity';
 import { ActivityService } from '../../services/activity.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpParams } from '@angular/common/http';
+import { Categorie } from '../../classes/categorie';
 
 @Component({
   selector: 'app-activity-list',
@@ -12,6 +13,9 @@ import { HttpParams } from '@angular/common/http';
 export class ActivityListComponent implements OnInit {
   activities!: Activity[];
   Form!: FormGroup;
+  categories: Categorie[] = [];
+  // loading states
+  isPageLoading: boolean = false;
   isSubmitting: boolean = false;
 
   constructor(
@@ -20,30 +24,65 @@ export class ActivityListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    // set loading page to true
+    this.isPageLoading = true;
+
+    // init form
     this.Form = this.fb.group({
+      // names are choosen for the backend specifications
       title__icontains: [''],
+      categories__name: [''],
     });
 
+    // get all categories
+    this.activityService.getAllCategories().subscribe({
+      next: (categories) => {
+        // set categories
+        this.categories = categories;
+        // set loading page to false
+        this.isPageLoading = false;
+      },
+      error: (error) => {
+        // set loading page to false
+        this.isPageLoading = false;
+      },
+    });
+
+    // get all activities
     this.queryActivities();
   }
 
-  queryActivities(title__icontains: string = '') {
+  queryActivities(
+    title__icontains: string = '',
+    categories__name: string = ''
+  ) {
+    // set loading state to true
     this.isSubmitting = true;
-    const params = new HttpParams().set('title__icontains', title__icontains);
+
+    // set params
+    const params = new HttpParams()
+      .set('title__icontains', title__icontains)
+      .set('categories__name', categories__name);
+
     this.activityService.getAllActivities(params).subscribe({
       next: (activities) => {
+        // set activities
         this.activities = activities;
+        // set loading state to false
         this.isSubmitting = false;
       },
-      error: (err) => {
-        console.log(err);
+      error: (error) => {
+        // set loading state to false
         this.isSubmitting = false;
       },
     });
   }
 
   onSubmit() {
-    const { title__icontains } = this.Form.value;
-    this.queryActivities(title__icontains);
+    // get title__icontains
+    const { title__icontains, categories__name } = this.Form.value;
+
+    // query activities
+    this.queryActivities(title__icontains, categories__name);
   }
 }
