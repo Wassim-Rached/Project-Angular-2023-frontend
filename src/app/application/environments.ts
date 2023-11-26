bac2f30dfb21ebe6e711ee63cc80f4a24e6155de
Wassim-Rached <wassimrached404@gmail.com>
2023-11-26 21:10:17 +0100

    Update API URLs and redirects in environments.ts

--- src/app/application/environments.ts
+++ src/app/application/environments.ts
@@ -1,10 +1,11 @@
 export const environment = {
-  // BASE_API_URL: 'http://localhost:8000/api/',
+  // API's
   BASE_API_URL: 'https://mini-project-iset.onrender.com/api/',
+  // redirects
   DEFAULT_REDIRECT_AFTER_LOGIN: '/',
   DEFAULT_REDIRECT_AFTER_LOGOUT: '/',
   DEFAULT_REDIRECT_AFTER_LOGIN_ADMIN: '/dashboard',
   DEFAULT_REDIRECT_FORBIDDEN: '/',
-  DEFAULT_REDIRECT_AFTER_ACTIVITY_UPDATE: '/dashboard/activities',
+  // names
   ACCESS_TOKEN_NAME: 'x-accessToken',
 };
